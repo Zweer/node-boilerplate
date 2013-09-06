f0d41c30a8febe2f3506bd852befc644e67e44a7
Niccolò Olivieri <flicofloc@gmail.com>
2013-09-07 00:12:11 +0200

    Added the winston log module

--- app/server.js
+++ app/server.js
@@ -1,7 +1,11 @@
 var express = require('express')
-  , config  = require('config');
+  , config  = require('config')
+  , winston = require('winston');
 
 module.exports = app = express();
+logger = new (winston.Logger)()
+  .add(winston.transports.Console, {
+    colorize: true
+  });
 
 isDevelopment = app.get('env') === 'development';
-
