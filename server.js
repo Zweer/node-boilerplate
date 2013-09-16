d02091992fe6a046ae4e67fb73285f0a647e8003
Niccolo' Olivieri <flicofloc@gmail.com>
2013-09-16 17:15:51 +0200

    Added comments to the code

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+/**
+ * Instantiate third-part libraries:
+ * - express: the framework
+ * - winston: the logger
+ * - our application configuration
+ */
 var express     = require('express')
   , winston     = require('winston')
   , application = require('./lib/app');
 
+/**
+ * Define global variables:
+ * - the application
+ * - the logger
+ * - if we are in development
+ */
 module.exports = app = express();
 logger = new (winston.Logger)()
   .add(winston.transports.Console, {
     colorize: true
   })
   .setLevels(winston.config.syslog.levels);
-
 isDevelopment = app.get('env') === 'development';
 
+/**
+ * Setup the application
+ */
 application.setup();
 application.clusterize();
 
+/**
+ * Setup the routes
+ */
 require('./routes');
